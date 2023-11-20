24d9fec4dfc1c78ae400ef4d88dcc2e176b635bf
Damien Bose <damienbose3@gmail.com>
2023-11-20 15:34:38 +0000

    Update run to include GP

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,10 +1,14 @@
-# Navigate to magpie root
-cd ..
+# Go to the root of this project
+cd $(git rev-parse --show-toplevel)
 
 # Clean previous experiments
 rm -rf _magpie_logs
 rm -rf _magpie_work
 
-# Run new expiriements
-python3 -m bin.local_search --scenario experiments/scenario/triangle_uniform.txt
-python3 -m bin.local_search --scenario experiments/scenario/triangle_weighted.txt
+# Experiments: Local Search
+python3 -m bin.local_search --scenario experiments/scenario/triangle_uniform.txt --algo FirstImprovement
+python3 -m bin.local_search --scenario experiments/scenario/triangle_weighted.txt --algo FirstImprovement
+
+# Experiments: Genetic Programming
+python3 -m bin.genetic_programming --scenario experiments/scenario/triangle_uniform.txt --algo GeneticProgrammingUniformConcat
+python3 -m bin.genetic_programming --scenario experiments/scenario/triangle_weighted.txt --algo GeneticProgrammingUniformConcat
