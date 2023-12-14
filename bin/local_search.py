5317e31b4e59c6a12b94d8cc4bc84b2eef921fd7
Damien Bose <damienbose3@gmail.com>
2023-12-14 23:51:33 +0000

    Seed

--- bin/local_search.py
+++ bin/local_search.py
@@ -12,7 +12,8 @@ import magpie
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Magpie local search')
     parser.add_argument('--scenario', type=pathlib.Path, required=True)
-    parser.add_argument('--algo', type=str)
+    parser.add_argument('--algo', type=str, required=True)
+    parser.add_argument('--output_dir', type=pathlib.Path)
     parser.add_argument('--seed', type=int)
     args = parser.parse_args()
 
@@ -33,6 +34,10 @@ if __name__ == "__main__":
         config['search']['algorithm'] = 'FirstImprovement'
         algo = magpie.algo.FirstImprovement
 
+    # Set the seed
+    if args.seed is not None:
+        config['magpie']['seed'] = str(args.seed)
+        
     # setup protocol
     magpie.bin.setup(config)
     protocol = magpie.bin.protocol_from_string(config['search']['protocol'])()
