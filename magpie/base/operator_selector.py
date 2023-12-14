e3023d0746e885bdcb098cffc07f576800b10226
Damien Bose <damienbose3@gmail.com>
2023-12-14 20:58:04 +0000

    Add sanity checks

--- magpie/base/operator_selector.py
+++ magpie/base/operator_selector.py
@@ -44,12 +44,24 @@ class EpsilonGreedy(AbstractOperatorSelector):
 
         self._average_qualities = {op: 0 for op in self._operators}
         self._action_count = {op: 0 for op in self._operators}
+
+        # Sanity checks
+        self.update_call_count = 0
+        self.select_call_count = 0
         
     def update_quality(self, operator, reward): # Note: reward = -fitness, since we're trying to minimize fitness
         self._action_count[operator] += 1
         self._average_qualities[operator] += (reward - self._average_qualities[operator]) / self._action_count[operator]
 
+        # Sanity checks
+        self.select_call_count += 1
+        assert self.update_call_count == self.select_call_count
+
     def select(self):
+        # Sanity checks
+        assert self.update_call_count == self.select_call_count
+        self.select_call_count += 1
+
         if random.random() < 1 - self._epsilon:
             return max(self._operators, key=lambda op: self._average_qualities[op])
         else:
