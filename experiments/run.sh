7b9a7f297f428f4dd0cef6ba181119e6bdcd2405
Damien Bose <damienbose3@gmail.com>
2023-12-15 00:48:47 +0000

    Running with random walk

--- experiments/run.sh
+++ experiments/run.sh
@@ -8,11 +8,23 @@ rm -rf _magpie_logs
 rm -rf _magpie_work
 rm -rf experiments/results
 
+# Where to put experiment output
+result_dir="experiments/results"
+
 # Experiments: Local Search
 for i in {1..1}
 do
-    python3 -m bin.local_search --scenario experiments/scenario/test_example.txt --algo RandomSearch --seed $i --output_dir experiments/results/epsilon_greedy/random_search/trial_$i
+    # RandomSearch: baseline and e-greedy
+    python3 -m bin.local_search --scenario experiments/scenario/baseline.txt --algo RandomSearch --seed $i --output_dir $result_dir/baseline/RandomSearch/trial_$i
+    python3 -m bin.local_search --scenario experiments/scenario/e-greedy.txt --algo RandomSearch --seed $i --output_dir $result_dir/epsilon_greedy/RandomSearch/trial_$i
+    
+    # RandomWalk: baseline and e-greedy
+    python3 -m bin.local_search --scenario experiments/scenario/baseline.txt --algo RandomWalk --seed $i --output_dir $result_dir/baseline/RandomWalk/trial_$i
+    python3 -m bin.local_search --scenario experiments/scenario/e-greedy.txt --algo RandomWalk --seed $i --output_dir $result_dir/epsilon_greedy/RandomWalk/trial_$i
 done
+
+# Wait for all child processes to complete
+wait
 
 # Parse results and produce summary statistics
 python3 -m experiments.parse_results --result_dir experiments/results
