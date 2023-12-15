2f30c269041dff6d516e04af0cc88ba1208d7db2
Damien Bose <damienbose3@gmail.com>
2023-12-15 01:21:55 +0000

    e-greedy proof of concept

--- experiments/run.sh
+++ experiments/run.sh
@@ -21,10 +21,20 @@ do
     # RandomWalk: baseline and e-greedy
     python3 -m bin.local_search --scenario experiments/scenario/baseline.txt --algo RandomWalk --seed $i --output_dir $result_dir/baseline/RandomWalk/trial_$i
     python3 -m bin.local_search --scenario experiments/scenario/e-greedy.txt --algo RandomWalk --seed $i --output_dir $result_dir/epsilon_greedy/RandomWalk/trial_$i
+
+    # BestImprovementNoTabu: baseline and e-greedy
+    python3 -m bin.local_search --scenario experiments/scenario/baseline.txt --algo BestImprovementNoTabu --seed $i --output_dir $result_dir/baseline/BestImprovementNoTabu/trial_$i
+    python3 -m bin.local_search --scenario experiments/scenario/e-greedy.txt --algo BestImprovementNoTabu --seed $i --output_dir $result_dir/epsilon_greedy/BestImprovementNoTabu/trial_$i
+
+    # FirstImprovementNoTabu: baseline and e-greedy
+    python3 -m bin.local_search --scenario experiments/scenario/baseline.txt --algo FirstImprovementNoTabu --seed $i --output_dir $result_dir/baseline/FirstImprovementNoTabu/trial_$i
+    python3 -m bin.local_search --scenario experiments/scenario/e-greedy.txt --algo FirstImprovementNoTabu --seed $i --output_dir $result_dir/epsilon_greedy/FirstImprovementNoTabu/trial_$i
 done
 
 # Wait for all child processes to complete
 wait
+
+echo "All experiments complete!"
 
 # Parse results and produce summary statistics
 python3 -m experiments.parse_results --result_dir experiments/results
