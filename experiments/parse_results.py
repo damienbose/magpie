6998f339044cb0d1cea1ec500b39f4467c14b7bc
Damien Bose <damienbose3@gmail.com>
2023-12-20 11:30:35 +0000

    New results

--- experiments/parse_results.py
+++ experiments/parse_results.py
@@ -6,12 +6,39 @@ import glob
 import pickle
 import magpie
 
-def parse_results(result, output_dir):
+def process_trial(trial_result, output_dir):
     with open(output_dir / "result.txt", "w") as f:
         print(f"Results Summary", file=f)
-        print(f"Starting Fitness: {result['initial_fitness']}", file=f)
-        print(f"Best Fitness: {result['best_fitness']}", file=f)
+        print(f"Starting Fitness: {trial_result['initial_fitness']}", file=f)
+        print(f"Best Fitness: {trial_result['best_fitness']}", file=f)
 
+def process_rl_algorithm(directory):
+    # Get the name of the algorithm
+    algorithm_name = directory.split("/")[-2]
+    print(f"Processing {algorithm_name}...")
+
+    # Get the list of subdirectories in the results directory
+    subdirectories = glob.glob(directory + '/*/')
+
+    # Iterate over each subdirectory
+    for trial_dir in subdirectories:
+        pickle_file = Path(trial_dir) / "logs/raw_result.pkl"
+
+        with open(pickle_file, "rb") as f:
+            trial_result_obj = pickle.load(f)
+        
+        process_trial(trial_result_obj, Path(trial_dir))
+
+def process_search_algorithm(directory):
+    # Get the name of the algorithm
+    algorithm_name = directory.split("/")[-2]
+    print(f"Processing {algorithm_name}...")
+
+    subdirectories = glob.glob(directory + '/*/')
+
+    # Iterate over each subdirectory
+    for subdirectory in subdirectories:
+        process_rl_algorithm(subdirectory)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -21,18 +48,8 @@ if __name__ == "__main__":
     result_output_path = Path(args.result_dir)
 
     # Get the list of subdirectories in the results directory
-    subdirectories = glob.glob(args.result_dir + '/*/*/*/*/')
+    subdirectories = glob.glob(args.result_dir + '/*/')
 
     # Iterate over each subdirectory
     for subdirectory in subdirectories:
-        # Check if the subdirectory is named "logs"
-        if subdirectory.endswith('logs/'):
-            # Get the path to the pickle file
-            pickle_file = Path(subdirectory) / 'raw_result.pkl'
-
-            # Load the pickle file
-            with open(pickle_file, 'rb') as f:
-                result = pickle.load(f)
-
-            # Parse the results
-            parse_results(result, Path(subdirectory).parent)
+        process_search_algorithm(subdirectory)
--- experiments/run.sh
+++ experiments/run.sh
@@ -5,7 +5,7 @@ cd $(git rev-parse --show-toplevel)
 
 
 # Where to put experiment output
-result_dir="experiments/results_e_greedy_proof_of_concept"
+result_dir="experiments/results"
 
 # Clear previous experiments
 rm -rf _magpie_logs
@@ -21,23 +21,15 @@ start_time=$(date +%s)
 
 
 # Experiments: Local Search
-for i in {1..10}
+for i in {1..2}
 do
     # RandomSearch: baseline and e-greedy
-    python3 -m bin.local_search --scenario experiments/scenario/baseline.txt --algo RandomSearch --seed $i --output_dir $result_dir/baseline/RandomSearch/trial_$i
-    python3 -m bin.local_search --scenario experiments/scenario/e-greedy.txt --algo RandomSearch --seed $i --output_dir $result_dir/epsilon_greedy/RandomSearch/trial_$i
-    
-    # RandomWalk: baseline and e-greedy
-    python3 -m bin.local_search --scenario experiments/scenario/baseline.txt --algo RandomWalk --seed $i --output_dir $result_dir/baseline/RandomWalk/trial_$i
-    python3 -m bin.local_search --scenario experiments/scenario/e-greedy.txt --algo RandomWalk --seed $i --output_dir $result_dir/epsilon_greedy/RandomWalk/trial_$i
+    python3 -m bin.local_search --scenario experiments/scenario/baseline.txt --algo RandomSearch --seed $i --output_dir $result_dir/RandomSearch/baseline/trial_$i
+    python3 -m bin.local_search --scenario experiments/scenario/e-greedy.txt --algo RandomSearch --seed $i --output_dir $result_dir/RandomSearch/epsilon_greedy/trial_$i
 
     # BestImprovementNoTabu: baseline and e-greedy
-    python3 -m bin.local_search --scenario experiments/scenario/baseline.txt --algo BestImprovementNoTabu --seed $i --output_dir $result_dir/baseline/BestImprovementNoTabu/trial_$i
-    python3 -m bin.local_search --scenario experiments/scenario/e-greedy.txt --algo BestImprovementNoTabu --seed $i --output_dir $result_dir/epsilon_greedy/BestImprovementNoTabu/trial_$i
-
-    # FirstImprovementNoTabu: baseline and e-greedy
-    python3 -m bin.local_search --scenario experiments/scenario/baseline.txt --algo FirstImprovementNoTabu --seed $i --output_dir $result_dir/baseline/FirstImprovementNoTabu/trial_$i
-    python3 -m bin.local_search --scenario experiments/scenario/e-greedy.txt --algo FirstImprovementNoTabu --seed $i --output_dir $result_dir/epsilon_greedy/FirstImprovementNoTabu/trial_$i
+    python3 -m bin.local_search --scenario experiments/scenario/baseline.txt --algo BestImprovementNoTabu --seed $i --output_dir $result_dir/BestImprovementNoTabu/baseline/trial_$i
+    python3 -m bin.local_search --scenario experiments/scenario/e-greedy.txt --algo BestImprovementNoTabu --seed $i --output_dir $result_dir/BestImprovementNoTabu/epsilon_greedy/trial_$i
 done
 
 # Wait for all child processes to complete
