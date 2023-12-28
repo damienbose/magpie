b8e5dc33e6d677eb0ff7c78172c18a40430b45f2
Damien Bose <damienbose3@gmail.com>
2023-12-28 22:10:08 +0000

    Data

--- experiments/automate_run.py
+++ experiments/automate_run.py
@@ -7,18 +7,28 @@ scenarios = [
 ]
 
 def set_up(args):
-    # Define the source and destination paths
-    source_path = '/path/to/source/file.ext'
-    destination_path = '/path/to/destination/file.ext'
-    shutil.copy(source_path, destination_path)
+    # Reset
+    shutil.rmtree("experiments/benchmark", ignore_errors=True)
+    shutil.rmtree("experiments/minisat", ignore_errors=True)
+
+    # Copy the data
+    shutil.copytree("examples/code/minisat/data/sat_uniform", "experiments/benchmark")
+    shutil.copy("examples/code/minisat/data/sat_uniform.py", "experiments/benchmark/sat_uniform.py")
+
+    # Copy code and remove the data subdirectory
+    shutil.copytree("examples/code/minisat", "experiments/minisat")
+    shutil.rmtree("experiments/minisat/data")
 
 
+    print("done")
+
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--step', type=str, default='check',
-                        choices=['setup', 'training', 'test'])
-    parser.add_argument('--seed', type=int, default=123)
+    parser.add_argument('--step', type=str, default='setup',
+                        choices=['setup', 'train', 'test'])
+    parser.add_argument('--seed', type=int, default=42)
     args = parser.parse_args()
 
     if args.step == 'setup':
