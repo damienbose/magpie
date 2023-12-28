28c8b44717c596febe5507401ad5b03fdb67bc57
Damien Bose <damienbose3@gmail.com>
2023-12-28 22:30:17 +0000

    todo cross val

--- experiments/automate_run.py
+++ experiments/automate_run.py
@@ -1,5 +1,5 @@
 import argparse
-import shutil
+import json
 
 scenarios = [
     "baseline.txt",
@@ -7,28 +7,24 @@ scenarios = [
 ]
 
 def set_up(args):
-    # Reset
-    shutil.rmtree("experiments/benchmark", ignore_errors=True)
-    shutil.rmtree("experiments/minisat", ignore_errors=True)
-
-    # Copy the data
-    shutil.copytree("examples/code/minisat/data/sat_uniform", "experiments/benchmark")
-    shutil.copy("examples/code/minisat/data/sat_uniform.py", "experiments/benchmark/sat_uniform.py")
-
-    # Copy code and remove the data subdirectory
-    shutil.copytree("examples/code/minisat", "experiments/minisat")
-    shutil.rmtree("experiments/minisat/data")
+    # TODO: Split into buckets for k-fold cross validation
 
 
-    print("done")
+
+
+
+
+    cross_val_split = None
+    with open(f'experiments/{args.results_dir}/cross_val_split.json', 'w') as file:
+        json.dump(cross_val_split, file, indent=4)
 
     
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--step', type=str, default='setup',
-                        choices=['setup', 'train', 'test'])
+    parser.add_argument('--step', type=str, default='setup', choices=['setup', 'train', 'test'])
     parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--results_dir', type=str, default='results')
     args = parser.parse_args()
 
     if args.step == 'setup':
