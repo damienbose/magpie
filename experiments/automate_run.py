95e06859b81d90bdf7c55e282b822b3e0f65f55f
Damien Bose <damienbose3@gmail.com>
2023-12-30 20:00:52 +0000

    Run without the test set

--- experiments/automate_run.py
+++ experiments/automate_run.py
@@ -1,5 +1,6 @@
 import argparse
 import importlib
+import traceback
 import automate_run_utils as utils
 importlib.reload(utils) # Reload instead of using cached version
 
@@ -9,10 +10,13 @@ num_replications = 1
 
 scenarios = [
     "baseline.txt",
-    "e-greedy.txt"
+    # "e-greedy.txt"
 ]
 
-search_algos = ['RandomSearch', 'BestImprovementNoTabu']
+search_algos = [
+    'RandomSearch',
+    # 'BestImprovementNoTabu'
+]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -24,11 +28,15 @@ if __name__ == '__main__':
     # Seed
     utils.seed(args.seed)
 
-    if args.step == 'setup':
-        # Generate the cross-validation split
-        utils.setup(args, num_folds, num_replications)
-    elif args.step == 'train':
-        # Run GI on the training set
-        utils.train(args, scenarios, search_algos)
-    elif args.step == 'test':
-        pass
+    try:
+        if args.step == 'setup':
+            # Generate the cross-validation split
+            utils.setup(args, num_folds, num_replications)
+        elif args.step == 'train':
+            # Run GI on the training set
+            utils.train(args, scenarios, search_algos)
+        elif args.step == 'test':
+            pass
+    except Exception:
+        with open(f"{args.results_dir}/error_{args.step}.txt", 'w') as f:
+            print(traceback.format_exc(), file=f)
