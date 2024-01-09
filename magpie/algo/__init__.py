ab0c565e27aa4dc409245c0d54ff73d6f308648b
Damien Bose <damienbose3@gmail.com>
2024-01-09 17:03:44 +0000

    No Tabu variants

--- magpie/algo/__init__.py
+++ magpie/algo/__init__.py
@@ -1,5 +1,6 @@
 from .local_search import LocalSearch, DummySearch, RandomSearch, RandomWalk, DebugSearch
 from .local_search import FirstImprovement, BestImprovement, WorstImprovement, TabuSearch
+from .local_search import FirstImprovementNoTabu, BestImprovementNoTabu
 from .genetic_programming import GeneticProgramming, GeneticProgrammingConcat, GeneticProgramming1Point, GeneticProgramming2Point, GeneticProgrammingUniformConcat, GeneticProgrammingUniformInter
 from .validation import ValidSearch, ValidSingle, ValidTest, ValidMinify
 from .ablation import AblationAnalysis
@@ -8,6 +9,7 @@ from .ablation import AblationAnalysis
 algos = [
     DummySearch, RandomSearch, RandomWalk, DebugSearch,
     FirstImprovement, BestImprovement, WorstImprovement, TabuSearch,
+    FirstImprovementNoTabu, BestImprovementNoTabu,
     GeneticProgrammingConcat, GeneticProgramming1Point, GeneticProgramming2Point, GeneticProgrammingUniformConcat, GeneticProgrammingUniformInter,
     ValidSingle, ValidTest, ValidMinify,
     AblationAnalysis,
