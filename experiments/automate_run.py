94ab3691c6934e6f93d3c98e7d2b4ad274adff35
Damien Bose <damienbose3@gmail.com>
2024-01-09 15:16:46 +0000

    Automate run

---
+++ experiments/automate_run.py
@@ -0,0 +1,44 @@
+import argparse
+import importlib
+import traceback
+import automate_run_utils as utils
+importlib.reload(utils) # Reload instead of using cached version
+
+# Cross validation setup
+num_folds = 5
+num_replications = 1
+
+operator_selectors = [
+    'UniformSelector',
+    # 'WeightedSelector',
+    # 'EpsilonGreedy'
+]
+
+search_algos = [
+    'RandomSearch',
+    # 'BestImprovementNoTabu'
+]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--step', type=str, default='setup', choices=['setup', 'train', 'test'])
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--results_dir', type=str, default='experiments/results')
+    args = parser.parse_args()
+
+    # Seed
+    utils.seed(args.seed)
+
+    try:
+        if args.step == 'setup':
+            # Generate the cross-validation split
+            utils.setup(args, num_folds, num_replications, operator_selectors, search_algos)
+        elif args.step == 'train':
+            # Run GI on the training set
+            utils.train(args, operator_selectors, search_algos)
+        elif args.step == 'test':
+            pass
+    except Exception:
+        with open(f"{args.results_dir}/error_{args.step}.txt", 'w') as f:
+            print(traceback.format_exc(), file=f)
+        raise
---
+++ experiments/automate_run_utils.py
@@ -0,0 +1,109 @@
+import random
+import json
+import subprocess
+import configparser
+from pathlib import Path
+
+def seed(seed):
+    random.seed(seed)
+
+def split_into_equal_random_subsets(arr, k):
+
+    arr_temp = arr.copy()
+
+    # Ensure the array can be divided into k equal subsets
+    n = len(arr_temp)
+    if n % k != 0:
+        raise ValueError("Array size is not divisible by k")
+
+    # Shuffle the array
+    random.shuffle(arr_temp)
+
+    # Calculate the size of each subset
+    subset_size = n // k
+
+    # Split the array into k subsets
+    subsets = [arr_temp[i*subset_size:(i+1)*subset_size] for i in range(k)]
+    return subsets
+
+
+def cross_val_setup(args, num_folds=5, num_replications=5):
+    # Generate the folds
+    with open('experiments/benchmark/sat_uniform.json', 'r') as file:
+        bins = json.load(file)
+
+    folds = [[] for _ in range(num_folds)]
+
+    for bin in bins:
+        mini_fold = split_into_equal_random_subsets(bin, num_folds)
+        for i in range(num_folds):
+            folds[i] += mini_fold[i]
+        
+    assert num_replications <= num_folds
+
+    # Generate the replications
+    replications = {}
+    for replication_num in range(num_replications):
+        test_folds = [replication_num]
+        train_folds = [i for i in range(num_folds) if i not in test_folds]
+        replications[replication_num] = {"train_folds" : train_folds, "test_folds" : test_folds}
+    
+    cross_validation_setup = {}
+    cross_validation_setup['num_folds'] = num_folds
+    cross_validation_setup['num_replications'] = num_replications
+    cross_validation_setup['folds'] = {i : folds[i] for i in range(num_folds)}
+    cross_validation_setup['replications'] = replications
+
+    with open(f'{args.results_dir}/cross_val_setup.json', 'w') as file:
+        json.dump(cross_validation_setup, file, indent=4)
+    
+    return cross_validation_setup
+
+def set_operator_selector_config(config, operator_selector):
+    config["search"]["operator_selector"] = operator_selector
+
+def set_batch_config(config, replication_num, cross_validation_setup):
+    if cross_validation_setup is not None:
+        train_folds = [cross_validation_setup['folds'][i] for i in cross_validation_setup['replications'][replication_num]['train_folds']]
+        config["search"]["batch_sample_size"] = str(sum([len(fold) for fold in train_folds]))
+        for i in range(len(train_folds)):
+            train_folds[i] = '\n'.join(train_folds[i])
+        train_folds = '\n' + '\n___\n'.join(train_folds)
+        config["search"]["batch_instances"] = train_folds
+    else:
+        pass # TODO: Finalise crossvalidation to address to delete this case. Here we simply follow the template
+
+def scenario_config_setup(args, operator_selectors, search_algos, num_replications, cross_validation_setup):
+    for operator_selector in operator_selectors:
+        for algo in search_algos:
+            for replication_num in range(num_replications):
+                config = configparser.ConfigParser()
+                config.read("experiments/scenario/template.txt")
+                set_operator_selector_config(config, operator_selector)
+                set_batch_config(config, replication_num, cross_validation_setup)
+                path = f"{args.results_dir}/{algo}/{operator_selector}/trial_{replication_num}/scenario.txt"
+                Path(path).parent.mkdir(parents=True, exist_ok=True)
+                with open(path, 'w') as configfile:
+                    config.write(configfile)
+
+def setup(args, num_folds, num_replications, operator_selectors, search_algos):
+    try: 
+        cross_validation_setup = cross_val_setup(args, num_folds, num_replications)
+    except:
+        cross_validation_setup = None # TODO: Finalise crossvalidation to address to delete this case. 
+    scenario_config_setup(args, operator_selectors, search_algos, num_replications, cross_validation_setup)
+
+def train(args, operator_selectors, search_algos):
+    # Load the cross validation setup
+    with open(f'{args.results_dir}/cross_val_setup.json', 'r') as file:
+        cross_validation_setup = json.load(file)
+    
+    num_replications = cross_validation_setup['num_replications']
+    for i in range(num_replications):
+        # Train on the training folds
+        for operator_selector in operator_selectors:
+            for algo in search_algos:
+                scenario = f"{args.results_dir}/{algo}/{operator_selector}/trial_{i}/scenario.txt"
+                command = f"python3 -m bin.local_search --scenario {scenario} --algo {algo} --seed {i} --output_dir {args.results_dir}/{algo}/{operator_selector}/trial_{i}"
+                print(command)
+                subprocess.run(command, shell=True, text=True)
