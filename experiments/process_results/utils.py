38138bbc979162f3b97722b30f954f47e687767e
Damien Bose <damienbose3@gmail.com>
2024-01-14 21:59:43 +0000

    Process results for efficiency

--- experiments/process_results/utils.py
+++ experiments/process_results/utils.py
@@ -15,3 +15,24 @@ def get_patch(log_files_folder):
         return patch
     except FileNotFoundError: # No patches found (i.e. variants with improved fitness)
         return None
+
+def get_num_variants_evaluated(log_files_folder):
+    # TODO: hard coded for now change once the experiment_report is updated
+    op_selector = log_files_folder.split("/")[-4]
+    if op_selector == "EpsilonGreedy":
+        return 2261
+    elif op_selector == "ProbabilityMatching":
+         return 1631
+    elif op_selector == "UCB":
+         return 1221
+    elif op_selector == "UniformSelector":
+         return 1965
+    return -1
+
+def get_num_successful_variants_evaluated(row):
+    log_files_folder = row['path_log']
+    num_warmup = row['pkl_obj']['config']["warmup"]
+    with open(log_files_folder + "experiment.log", "r") as f:
+        logs = f.read()
+    num_success = logs.count("EXEC>  SUCCESS")
+    return num_success - num_warmup
