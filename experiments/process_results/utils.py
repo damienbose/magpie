5c26d3b22e50bcc43f459e35f9cff335de8c74b6
Damien Bose <damienbose3@gmail.com>
2024-01-17 20:27:48 +0000

    Generic utils

--- experiments/process_results/utils.py
+++ experiments/process_results/utils.py
@@ -16,23 +16,15 @@ def get_patch(log_files_folder):
     except FileNotFoundError: # No patches found (i.e. variants with improved fitness)
         return None
 
-def get_num_variants_evaluated(log_files_folder):
-    # TODO: hard coded for now change once the experiment_report is updated
-    op_selector = log_files_folder.split("/")[-4]
-    if op_selector == "EpsilonGreedy":
-        return 2261
-    elif op_selector == "ProbabilityMatching":
-         return 1631
-    elif op_selector == "UCB":
-         return 1221
-    elif op_selector == "UniformSelector":
-         return 1965
-    return -1
+def get_num_variants_evaluated(pkl_obj):
+    run_results = pkl_obj['run_results']
+    return len(run_results)
+     
 
-def get_num_successful_variants_evaluated(row):
-    log_files_folder = row['path_log']
-    num_warmup = row['pkl_obj']['config']["warmup"]
-    with open(log_files_folder + "experiment.log", "r") as f:
-        logs = f.read()
-    num_success = logs.count("EXEC>  SUCCESS")
-    return num_success - num_warmup
+def get_num_successful_variants_evaluated(pkl_obj):
+    run_results = pkl_obj['run_results']
+    num_success = 0
+    for result in run_results:
+        if result['status'] == 'SUCCESS':
+            num_success += 1
+    return num_success
