ca04674bca033271176bb918e30a532cefb4b086
Damien Bose <damienbose3@gmail.com>
2024-03-06 23:39:04 +0000

    Fix run

--- experiments/generate_coverage/run.sh
+++ experiments/generate_coverage/run.sh
@@ -3,4 +3,4 @@
 # Go to the root of this project
 cd $(git rev-parse --show-toplevel)
 
-python3 experiments/generate_coverage/run.py
+python3 experiments/generate_coverage/run.py &> experiments/generate_coverage/logs.txt 2>&1
