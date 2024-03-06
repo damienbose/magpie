1d2f0e58d52e59e5ac03041d62ba1a2e4720c82e
Damien Bose <damienbose3@gmail.com>
2024-03-06 22:45:10 +0000

    Get bins

---
+++ experiments/generate_coverage/run.py
@@ -0,0 +1,18 @@
+import subprocess
+import json
+import os
+
+if __name__ == "__main__":
+    # Generate the folds
+    with open('examples/code/benchmark/sat_uniform.json', 'r') as file:
+        bins = json.load(file)
+
+    # Use absolute paths for test cases & SAT/UNSAT label
+    root_dir = os.getcwd()
+    in_path = root_dir + "/examples/code/benchmark"
+    for bin in bins:
+        for i, elem in enumerate(bin):
+            test_case_type = 'SAT' if elem.split('/')[-1].count('u') == 1 else 'UNSAT'
+            bin[i] = f"{in_path}/{elem} {test_case_type}"
+    
+    print(bins)
