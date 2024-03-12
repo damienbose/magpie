3119b27c0b0f974c6f1468731bab350f0cd8a60d
Damien Bose <damienbose3@gmail.com>
2024-03-12 09:35:01 +0000

    Clean up

--- experiments/automate_run.py
+++ experiments/automate_run.py
@@ -24,7 +24,7 @@ operator_selectors = [
     'UniformSelector',
     # 'WeightedSelector',
     'EpsilonGreedy',
-    'ProbabilityMatching',1
+    'ProbabilityMatching',
     'UCB',
     'PolicyGradient'
 ]
