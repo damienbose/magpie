02de655899d3624e26fb0d76c61cf7b681f84894
Damien Bose <damienbose3@gmail.com>
2024-05-09 09:23:39 +0100

    Remove unused codepath since changed caching

--- magpie/bin/program.py
+++ magpie/bin/program.py
@@ -371,12 +371,6 @@ class BasicProgram(magpie.base.AbstractProgram):
 
         # final process
         self.process_batch_final(run_result)
-
-        if run_result.status == 'SUCCESS' and run_result.fitness is None:
-            # with open('weird_error.pkl', 'wb') as file:
-            #     pickle.dump(run_result, file)
-            # assert False, "What?"
-            run_result.status = 'WEIRD_ERROR'
         return run_result
 
     def process_init_exec(self, run_result, exec_result):
